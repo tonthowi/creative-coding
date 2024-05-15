4e80a8e111b2f989ce9bba1dea4f6a7c7d99f8bb
tonthowi <tonthowi@gmail.com>
2024-05-15 14:15:05 +0700

    Edit again *sigh*

--- src/sketchbook/randomCirculars.js
+++ src/sketchbook/randomCirculars.js
@@ -5,17 +5,18 @@ export function draw (canvas, context) {
 let radius;
 let x = 500;
 let y = 370;
-let startAngle = 0;
+let startAngle;
 let endAngle;
 
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 15; i++) {
     startAngle = Math.random();
     endAngle = Math.random();
-    radius = 50 * i;
+    radius = 20 * i;
+
     context.beginPath();
     context.arc(x, y, radius, startAngle, endAngle);
     context.stroke();
-    context.lineWidth = 10;
+    context.lineWidth = Math.random( 1, 7);
 }
 
 // Your drawing code ends here
